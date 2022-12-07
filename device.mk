7d0082c3802d9f04a30a17c03d7432b24938c504
chandu078 <chandudyavanapelli03@gmail.com>
2022-12-07 05:30:46 +0000

    Spacewar: Add NT camera support  * Camera repository: https://gitlab.com/ThankYouMario/proprietary_vendor_nothing_camera.
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -169,6 +169,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-service
 
 # Camera
+$(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
